8587a3accb84169ac5276c676172fb58abb7110f
Angelo Cordon <angelocordon@gmail.com>
2018-02-18 20:07:09 -0800

    Set up

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
